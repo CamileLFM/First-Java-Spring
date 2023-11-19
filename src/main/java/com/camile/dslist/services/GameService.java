5209c7aa6a9aa0b3638fce6deb8e839e6ada4b7d
Camile Lopes Franco de Macedo <149821917+CamileLFM@users.noreply.github.com>
2023-11-19 20:17:49 -0300

    Finished lesson 1.

--- src/main/java/com/camile/dslist/services/GameService.java
+++ src/main/java/com/camile/dslist/services/GameService.java
@@ -17,6 +17,7 @@ public class GameService {
 
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(GameMinDTO::new).toList();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
+
 }
